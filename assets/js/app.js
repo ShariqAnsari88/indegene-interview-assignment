01638c4dc47173c914388f00c909bd0611f0dcfa
ShariqAnsari88 <shariqatwork@gmail.com>
2020-08-05 21:58:39 +0530

    work till done

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,39 @@
-$(document).ready(function () {})
+$(document).ready(function () {
+  let resultTest = {
+    range: $("input[type=range]"),
+    triggerRange: function () {
+      $(".trigger-left").click(() => resultTest.range.val(1))
+      $(".trigger-right").click(() => resultTest.range.val(3))
+    },
+    timeIconAnim: function () {
+      $(".time-icon").hover(
+        function () {
+          $(this).siblings().addClass("disabled")
+        },
+        function () {
+          $(this).siblings().removeClass("disabled")
+        }
+      )
+      $(".time-icon").click(function () {
+        $(this).addClass("active")
+        $(this).parent().addClass("has-active")
+        $(this).siblings().addClass("disabled")
+        $(this).siblings().removeClass("active")
+      })
+    },
+    reset: function () {
+      $(".reset").click(function (e) {
+        e.preventDefault()
+        resultTest.range.val(1)
+        $(".time-icon").removeClass("active")
+        $(".time-icons-section").removeClass("has-active")
+      })
+    },
+    init: function () {
+      resultTest.triggerRange()
+      resultTest.timeIconAnim()
+      resultTest.reset()
+    }
+  }
+  resultTest.init()
+})
