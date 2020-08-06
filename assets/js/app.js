fe6e2b0a67efb8b4fc1d4239c70680457497ecb4
ShariqAnsari88 <shariqatwork@gmail.com>
2020-08-06 11:26:15 +0530

    This is the final commit - Assignment has been completed

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,19 +1,15 @@
 $(document).ready(function () {
   let resultTest = {
-
     range: $("input[type=range]"),
     mbSlider: $(".mb-slider"),
     isMobile: window.innerWidth <= 800,
 
     triggerRange: function () {
-
       $(".trigger-left").click(() => resultTest.range.val(1))
       $(".trigger-right").click(() => resultTest.range.val(3))
-
     },
 
     timeIconAnim: function () {
-
       $(".time-icon").hover(
         function () {
           $(this).siblings().addClass("disabled")
@@ -29,23 +25,19 @@ $(document).ready(function () {
         $(this).siblings().addClass("disabled")
         $(this).siblings().removeClass("active")
       })
-
     },
 
     reset: function () {
-
       $(".reset").click(function (e) {
         e.preventDefault()
         resultTest.range.val(1)
         $(".time-icon").removeClass("active")
         $(".time-icons-section").removeClass("has-active")
       })
-
     },
 
-    slick: function(el, slidesToShow, slidesToScroll, arrows, dots, centerMode, infinite, centerPadding){
-
-      $(".time-icon").each(function(ind, elm){
+    slick: function (el, slidesToShow, slidesToScroll, arrows, dots, centerMode, infinite, centerPadding) {
+      $(".time-icon").each(function (ind, elm) {
         $(elm).children().wrapAll("<div class='mb flex justify-content-center align-items-center flex-flow-column'>")
       })
 
@@ -57,20 +49,17 @@ $(document).ready(function () {
         centerMode: centerMode,
         infinite: infinite,
         centerPadding: centerPadding
-      });
-
+      })
     },
 
     init: function () {
-
       let windowHeight = window.innerHeight
-      $("body").css({"min-height":windowHeight})
+      $("body").css({ "min-height": windowHeight })
 
       resultTest.triggerRange()
-      if(resultTest.isMobile !== true) resultTest.timeIconAnim()
-      if(resultTest.isMobile === true) resultTest.slick(resultTest.mbSlider, 1, 1, false, true, true, true, '60px')
+      if (resultTest.isMobile !== true) resultTest.timeIconAnim()
+      if (resultTest.isMobile === true) resultTest.slick(resultTest.mbSlider, 1, 1, false, true, true, true, "60px")
       resultTest.reset()
-
     }
   }
   resultTest.init()
